4a2a075918fd5288c91937b9755672ccb1017818
Thibault-GX <thibault.grimoux@gmail.com>
2020-01-07 16:59:53 +0100

    Push de la correction du TP2, ajout des commentaires à venir.

--- TP2/index.php
+++ TP2/index.php
@@ -2,38 +2,94 @@
 $partNb = 10;
 $exerciseNb = 'TP 2';
 include '../header.php';
+$firstName = $lastName = $age = $society = $gender = '';
+$formSubmitted = false;
+$regexName = "/^[A-Za-zéÉ][A-Za-záàâäãåçéèêëíìîïñóòôöõúùûüýÿæœ]+((-| )[A-Za-záàâäãåçéèêëíìîïñóòôöõúùûüýÿæœ]+)?$/";
+$errors = [];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $formSubmitted = true;
+    $firstName = trim(filter_input(INPUT_POST, 'firstName', FILTER_SANITIZE_STRING));
+    if (empty($firstName)) {
+        $errors['firstName'] = 'Veuillez renseigner le prénom.';
+    } elseif (!preg_match($regexName, $firstName)) {
+        $errors['firstName'] = 'Votre prénom contient des caractères non autorisés !';
+    }
+    $gender = trim(filter_input(INPUT_POST, 'gender', FILTER_SANITIZE_STRING));
+    if (empty($gender)) {
+        $errors['gender'] = 'Veuillez renseigner votre civilité.';
+    } elseif (!preg_match('/woman|man/', $gender)) {
+        $errors['gender'] = 'Votre prénom contient des caractères non autorisés !';
+    }
+    //contôle du prénom
+    $lastName = trim(filter_input(INPUT_POST, 'lastName', FILTER_SANITIZE_STRING));
+    if (empty($lastName)) {
+        $errors['lastName'] = 'Veuillez renseigner le nom.';
+    } elseif (!preg_match($regexName, $lastName)) {
+        $errors['lastName'] = 'Votre nom contient des caractères non autorisés !';
+    }
+    //contôle du nom de la société
+    $society = trim(filter_input(INPUT_POST, 'society', FILTER_SANITIZE_STRING));
+    if (empty($society)) {
+        $errors['society'] = 'Veuillez renseigner le nom de la société.';
+    } elseif (!preg_match($regexName, $society)) {
+        $errors['society'] = 'Votre nom contient des caractères non autorisés !';
+    }
+    //contrôle de l'âge
+    $age = trim(filter_input(INPUT_POST, 'age', FILTER_SANITIZE_NUMBER_INT));
+    $options = array('options' => array('min_range' => 16, 'max_range' => 88));
+    if (empty($age)) {
+        $errors['age'] = 'Veuillez renseigner votre âge.';
+    } elseif (!filter_input(INPUT_POST, 'age', FILTER_VALIDATE_INT, $options)) {
+        $errors['age'] = 'Votre âge n\'est pas correct !';
+    }
+}
 ?>
 <form method="post" action="index.php" class="formParts" novalidate>
     <div class="form-group row">
-    <label for="gender" class="col-form-label col-4">Civilité :</label>
-    <select id="gender" name="gender" class="form-control col-8">
-        <option value="woman">
-            Madame
-        </option>
-        <option value="man">
-            Monsieur
-        </option>
-    </select>
+        <label for="gender" class="col-form-label col-2">Civilité :</label>
+        <select id="gender" name="gender" class="form-control col-8 custom-select">
+            <option value="">
+                Choisissez
+            </option>
+            <option value="woman">
+                Madame
+            </option>
+            <option value="man">
+                Monsieur
+            </option>
+        </select>
+        <span class="text-danger"><?= ($errors['gender']) ?? '' ?></span>
     </div>
     <div class="form-group row">
-        <label for="lastName" class="col-form-label col-4">Nom :</label>
-        <input id="lastName" name="lastName" type="text" value="" class="form-control col-8">
+        <label for="lastName" class="col-form-label col-2">Nom :</label>
+        <input id="lastName" name="lastName" type="text" value="<?= $lastName ?>" class="form-control col-8" placeholder="TOTO">
+        <span class="text-danger"><?= ($errors['lastName']) ?? '' ?></span>
     </div>
     <div class="form-group row">
-        <label for="firstName" class="col-form-label col-4">Prénom :</label>
-        <input id="firstName" name="firstName" type="text" value="" class="form-control col-8">
+        <label for="firstName" class="col-form-label col-2">Prénom :</label>
+        <input id="firstName" name="firstName" type="text" value="<?= $firstName ?>" class="form-control col-8" placeholder="Tata">
+        <span class="text-danger"><?= ($errors['firstName']) ?? '' ?></span>
     </div>
     <div class="form-group row">
-        <label for="age" class="col-form-label col-4">Âge :</label>
-        <input id="age" name="age" type="number" value="" class="form-control col-8">
+        <label for="age" class="col-form-label col-2">Âge :</label>
+        <input id="age" name="age" type="text" value="<?= $age ?>" class="form-control col-8" placeholder="20">
+        <span class="text-danger"><?= ($errors['age']) ?? '' ?></span>
     </div>
     <div class="form-group row">
-        <label for="society" class="col-form-label col-4">Société :</label>
-        <input id="society" name="society" type="text" value="" class="form-control col-8">
+        <label for="society" class="col-form-label col-2">Société :</label>
+        <input id="society" name="society" type="text" value="<?= $society ?>" class="form-control col-8" placeholder="Kicodedur">
+        <span class="text-danger"><?= ($errors['society']) ?? '' ?></span>
     </div>
     <div class="form-group row justify-content-center">
         <input type="submit" value="Inscription" class="btn btn-success">
     </div>
 </form>
+<?php if ($formSubmitted && count($errors) == 0) { ?>
+    <p><?= $gender ?></p>
+    <p><?= $lastName ?></p>
+    <p><?= $firstName ?></p>
+    <p><?= $age ?></p>
+    <p><?= $society ?></p>
 <?php
+}
 include '../footer.php';
